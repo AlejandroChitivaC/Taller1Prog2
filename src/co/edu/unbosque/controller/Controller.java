2da16ad8c074581ed25a68fbd38c951153da07f7
AlejandroChitivaC <achitivac@unbosque.edu.co>
2022-02-23 18:03:16 -0500

    4°requirement are added succesfully.

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,7 +1,11 @@
 package co.edu.unbosque.controller;
 
 import co.edu.unbosque.model.CSV;
+import co.edu.unbosque.model.Venta;
 import co.edu.unbosque.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The type Controller.
@@ -25,8 +29,19 @@ public class Controller {
 		System.out.println(csv.Summary());
 		System.out.println(csv.getCsvList().get(0).getQuantity());
 
-		String busqueda = view.pedirDato("Ingrese el InvoiceNo que desea buscar");
-		System.out.println(csv.findByInvoiceNo(busqueda));
+		List<Venta> encontrados;
+		do {
+			String busqueda = view.pedirDato("Ingrese el InvoiceNo que desea buscar");
+			encontrados = csv.findByInvoiceNo(busqueda);
+		} while(encontrados.size() == 0);
+		System.out.println(encontrados);
+// STOCKCODE
+		String stockCodes;
+		do {
+			String stockCode = view.pedirDato("Ingrese el StockCode que desea buscar");
+			stockCodes=csv.countByStockCode(stockCode);
+		}while (stockCodes.length()==0);
+		System.out.println(stockCodes);
 
 	}
 }
--- src/co/edu/unbosque/model/CSV.java
+++ src/co/edu/unbosque/model/CSV.java
@@ -10,100 +10,122 @@ import java.util.List;
  * The type Csv.
  */
 public class CSV {
-    private List<Venta> csvList;
+	private List<Venta> csvList;
 
-    /**
-     * Instantiates a new Csv.
-     *
-     * @param localpath the localpath
-     */
-    public CSV(String localpath) {
-        csvList = new ArrayList<>();
-        readFromPath(localpath);
-    }
+	/**
+	 * Instantiates a new Csv.
+	 *
+	 * @param localpath the localpath
+	 */
+	public CSV(String localpath) {
+		csvList = new ArrayList<>();
+		readFromPath(localpath);
+	}
 
-    /**
-     * Read from path.
-     *
-     * @param localpath the localpath
-     */
-    public void readFromPath(String localpath) {
-        System.out.println("Reading a CSV using OpenCSV library");
-        try {
-            CSVReader reader = new CSVReader(new FileReader(localpath));
-            String[] linea;
-            reader.readNext();
-            do {
-                linea = reader.readNext();
-                try {
-                    String invoiceNo = linea[0];
-                    String stockCode = linea[1];
-                    String description = linea[2];
-                    String quantity = linea[3];
-                    String invoiceDate = linea[4];
-                    String unitPrice = linea[5];
-                    String customerId = linea[6];
-                    String country = linea[7];
-                    Venta venta = new Venta(invoiceNo, stockCode, description, quantity, invoiceDate, unitPrice, customerId, country);
-                    csvList.add(venta);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } while ((linea != null));
-            reader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	/**
+	 * Read from path.
+	 *
+	 * @param localpath the localpath
+	 */
+	public void readFromPath(String localpath) {
+		System.out.println("Reading a CSV using OpenCSV library");
+		try {
+			CSVReader reader = new CSVReader(new FileReader(localpath));
+			String[] linea;
+			reader.readNext();
+			do {
+				linea = reader.readNext();
+				try {
+					String invoiceNo = linea[0];
+					String stockCode = linea[1];
+					String description = linea[2];
+					String quantity = linea[3];
+					String invoiceDate = linea[4];
+					String unitPrice = linea[5];
+					String customerId = linea[6];
+					String country = linea[7];
+					Venta venta = new Venta(invoiceNo, stockCode, description, quantity, invoiceDate, unitPrice, customerId, country);
+					csvList.add(venta);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} while ((linea != null));
+			reader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Summary float.
-     *
-     * @return the float
-     */
-    public float Summary(){
-        float total = 0;
-        for(int i=0;i<csvList.size();i++){
-            Venta aux=csvList.get(i);
-            float quantity=Float.parseFloat(aux.getQuantity());
-            float unitPrice=Float.parseFloat(aux.getUnitPrice());
-            total=total+(quantity*unitPrice);
-        }
-        return total;
-   }
+	/**
+	 * Summary float.
+	 *
+	 * @return the float
+	 */
+	public float Summary() {
+		float total = 0;
+		for (int i = 0; i < csvList.size(); i++) {
+			Venta aux = csvList.get(i);
+			float quantity = Float.parseFloat(aux.getQuantity());
+			float unitPrice = Float.parseFloat(aux.getUnitPrice());
+			total = total + (quantity * unitPrice);
+		}
+		return total;
+	}
 
-    /**
-     * Find by invoice no list.
-     *
-     * @param search the search
-     * @return the list
-     */
-    public List<Venta> findByInvoiceNo(String search) {
-        List<Venta> list = new ArrayList<>();
-       for (Venta aux : csvList) {
-           if (aux.getInvoiceNo().equals(search)) {
-               list.add(aux);
-           }
-       }
-       return list;
-   }
+	/**
+	 * Find by invoice no list.
+	 *
+	 * @param search the search
+	 * @return the list
+	 */
+	public List<Venta> findByInvoiceNo(String search) {
+		List<Venta> list = new ArrayList<>();
+		for (Venta aux : csvList) {
+			if (aux.getInvoiceNo().equals(search)) {
+				list.add(aux);
+			}
+		}
+		return list;
+	}
 
-    /**
-     * Gets csv list.
-     *
-     * @return the csv list
-     */
-    public List<Venta> getCsvList() {
-        return csvList;
-    }
+	public String countByStockCode(String stockCode) {
+
+		String solution = "";
+
+		int sellUnits = 0;
+		for (int i = 0; i < csvList.size(); i++) {
+			String stockCo = csvList.get(i).getStockCode();
 
 
-    /**
-     * Sets csv list.
-     *
-     * @param csvList the csv list
-     */
-    public void setCsvList(List<Venta> csvList) {
-        this.csvList = csvList;
-    }
+			if (stockCode.equalsIgnoreCase(stockCo)) {
+
+				int j = Integer.parseInt(csvList.get(i).getQuantity());
+				sellUnits = sellUnits + j;
+
+			}
+		}
+
+		System.out.println("la cantidad total de productos vendidos con el stockCode  " + stockCode + " es :  " + sellUnits);
+
+		return solution;
+	}
+
+	/**
+	 * Gets csv list.
+	 *
+	 * @return the csv list
+	 */
+	public List<Venta> getCsvList() {
+		return csvList;
+	}
+
+
+	/**
+	 * Sets csv list.
+	 *
+	 * @param csvList the csv list
+	 */
+	public void setCsvList(List<Venta> csvList) {
+		this.csvList = csvList;
+	}
 }
